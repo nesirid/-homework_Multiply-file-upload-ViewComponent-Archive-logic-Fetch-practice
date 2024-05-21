770467856193b6328e796a59a4e23bfcb40a85e9
nesirid <nasirid@code.edu.az>
2024-05-22 03:52:29 +0400

    first commit

---
+++ AspMVCHomeTask/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using AspMVCHomeTask.Data;
+using AspMVCHomeTask.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AspMVCHomeTask.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly ICategoryService _categoryService;
+
+        // Объединение конструкторов в один
+        public ProductController(AppDbContext context, ICategoryService categoryService)
+        {
+            _context = context;
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.ToListAsync();
+            return View(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RestoreFromArchive(int? id)
+        {
+            if (id == null) return BadRequest();
+
+            var category = await _categoryService.GetByIdAsync((int)id);
+
+            if (category == null) return NotFound();
+
+            category.SoftDeleted = false;
+            await _categoryService.UpdateAsync(category);
+
+            return Ok();
+        }
+    }
+}
---
+++ AspMVCHomeTask/Services/CategoryService.cs
@@ -0,0 +1,57 @@
+﻿using AspMVCHomeTask.Data;
+using AspMVCHomeTask.ViewModels.Categories;
+using AspMVCHomeTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using AspMVCHomeTask.Models;
+
+
+
+namespace AspMVCHomeTask.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<IEnumerable<CategoryProductVM>> GetAllWithProductCountAsync()
+        {
+            IEnumerable<Category> categories = await _context.Categories.Include(m => m.Products)
+                                                                        .ToListAsync();
+            return categories.Select(m => new CategoryProductVM
+            {
+                CategoryName = m.Name,
+                CreateDate = m.CreateDate.ToString("MM.dd.yyyy"),
+                Price = m.Price,
+                ProductCount = m.ProductCount
+            });
+        }
+        public async Task<Category> GetByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Category>> GetArchivedCategoriesAsync()
+        {
+            return await _context.Categories.Where(c => c.SoftDeleted).ToListAsync();
+        }
+
+    }
+
+
+ }
+
